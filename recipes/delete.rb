9b385d872fe1d3e0c0dd75bd298d0b8da70aa1ca
kgalli <mail@kgalli.de>
2015-08-01 22:09:58 +0200

    Add delete A record recipe

---
+++ recipes/delete.rb
@@ -0,0 +1,16 @@
+include_recipe 'route53'
+
+instance_name = node[:opsworks][:instance][:hostname]
+stack_name    = node[:opsworks][:stack][:name]
+instance_name = node[:opsworks][:instance][:hostname]
+domain        = node[:custom_domain]
+
+route53_record "delete a record" do
+  name  [instance_name, stack_name, domain].join('.')
+  type  "A"
+  zone_id               node[:dns_zone_id]
+  aws_access_key_id     node[:custom_access_key]
+  aws_secret_access_key node[:custom_secret_key]
+  action :delete
+end
+
